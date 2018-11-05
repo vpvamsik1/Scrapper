ce1a0eb1bf72e1320681c370940e5f8fc8edd7ff
Vamsi Krishna <vpvamsik1@gmail.com>
2018-11-05 11:20:33 -0800

    Commit 1

---
+++ public/app.js
@@ -0,0 +1,89 @@
+
+
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+    //   $("#articles").append("<p data-id='" + data[i]._id + "'>" + "</p>");
+    //   $("#P3").append(data[i].title);
+    //   $("#P4").append(data[i].link);
+    //   $("#P5").append(data[i].summary);
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "Title: " + data[i].title + "<br />" + "Link: " + data[i].link + "<br />" + "Summary: " + data[i].summary + "</p>");
+
+    }
+  });
+
+  $(document).on("click", "p", function() {
+    $("#notes").empty();
+    // alert("hello");
+    var id = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + id
+    }).then(function(data){
+        console.log(data);
+
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<h4>");
+        // $("#notes").append("<button id='delete'>Delete</button>");
+        $("#notes").append("<input id='titleinput' name='title' >");
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+        if (data.note) {
+            $("#titleinput").val("");
+            // $("#bodyinput").val(data.note.body);
+            $("h4").html("<span id='noteTitle'>" + data.note.title + "</span>" + "<button data-id='" + data._id + "' class='btn btn-danger' id='delete'>Delete</button>");
+
+            $(document).on("click", "#noteTitle", function() {
+                $("#bodyinput").val(data.note.body);
+            });
+        }
+    });
+});
+
+$(document).on("click", "#delete", function(){
+
+    var id = $(this).attr("data-id");
+    console.log(id);
+
+    $.ajax({
+        method: "DELETE",
+        url: "/articles/" + id,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+    
+        // On successful call
+        .then(function(data) {
+            console.log(data);
+            $("#notes").empty();
+        });
+        $("#titleinput").val("");
+        $("#bodyinput").val("");
+});
+
+// $("#savenote").on("click", function(){
+$(document).on("click", "#savenote", function() {
+    var id = $(this).attr("data-id");
+    // alert("hello");
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + id,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+
+    })
+    .then(function(data) {
+        console.log(data);
+        $("#notes").empty();
+    });
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+});
---
+++ server.js
@@ -0,0 +1,123 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+
+
+var id = mongoose.Types.ObjectId(id);
+
+app.get("/scrape", function(req, res){
+    axios.get("https://www.nytimes.com/section/opinion?pagetype=Homepage&action=click&module=Opinion").then(function(response){
+        var $ = cheerio.load(response.data);
+    
+        $("div.story-body").each(function(i, element) {
+          var result = {};
+    
+          result.title = $(this).find("a").text();
+          result.link = $(this).find("a").attr("href");
+          result.summary = $(this).find("p.summary").text();
+      
+          db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
+        });
+        res.send("Scrape Complete");
+      });
+      
+});
+
+app.get("/articles", function(req, res){
+    db.Article.find({})
+        .then(function(dbArticle){
+            res.json(dbArticle);
+        })
+        .catch(function(err){
+            res.json(err);
+        });
+});
+
+
+app.get("/articles/:id", function(req, res){
+    db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle){
+        res.json(dbArticle);
+    }).catch(function(err){
+        res.json(err);
+    });
+});
+  
+  // Route for saving/updating an Article's associated Note
+
+
+  app.post("/articles/:id", function(req, res){
+
+    db.Note.create(req.body)
+      .then(function(dbNote){
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle){
+          res.json(dbArticle);
+      })
+      .catch(function(err){
+        res.json(err);
+      });
+  });
+
+  app.delete("/articles/:id", function(req, res) {  
+    // Remove a note using the objectID
+    console.log('pinfo1');
+    db.Note.remove(
+        {
+          _id: req.params.id
+        }.then(function(dbNote){
+          res.json(dbNote);
+      }.then(function(error, removed) {
+        console.log('pinfo2');
+        // Log any errors from mongojs
+        if (error) {
+          console.log(error);
+          res.send(error);
+        }
+        else {
+          // Otherwise, send the mongojs response to the browser
+          // This will fire off the success function of the ajax request
+          console.log(removed);
+          res.send(removed);
+        }
+      })
+    
+    
+    )
+  )
+});
+
+
+
+  
+    
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
