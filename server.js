ac3932ab718351ec3fe04ff564f049434410fe77
Vamsi Krishna <vpvamsik1@gmail.com>
2019-02-14 11:11:35 -0800

    make it better

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI ||"mongodb://localhost/scraper"
+var MONGODB_URI = process.env.MONGODB_URI ||"mongodb://localhost/scrapper"
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
