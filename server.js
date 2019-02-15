da6dd3ffd5ca6b6c05a09ed4d78350fc1d196b05
Vamsi Krishna <vpvamsik1@gmail.com>
2019-02-15 13:09:39 -0800

    Change 12

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(express.static("public"));
 // var MONGODB_URI = "mongodb://localhost:27017/scraper"; 
 
 console.log("before url 2");
-var url = "mongodb://heroku_jsv18f6f:88d6bdf0mvsmmvtjmv9htunvu@ds151943.mlab.com:51943/heroku_jsv18f6f"
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/scraper";
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/scraper";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/scraper";
 // || "mongodb://heroku_jsv18f6f:88d6bdf0mvsmmvtjmv9htunvu@ds151943.mlab.com:51943/heroku_jsv18f6f";
 
 //var MONGODB_URI = "mongodb://heroku_jsv18f6f:88d6bdf0mvsmmvtjmv9htunvu@ds151943.mlab.com:51943/heroku_jsv18f6f";
@@ -45,8 +45,8 @@ MongoClient.connect(url, function (err, db) {
 });
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
-console.log(MONGODB_URI);
+mongoose.connect(url);
+console.log(url);
 
 console.log('after');
 
